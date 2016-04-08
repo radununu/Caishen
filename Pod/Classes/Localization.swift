0ded38003da5e2773877e157846ca2f63d14ab72
Shiyuan Jiang <James.JSY@gmail.com>
2016-04-08 15:05:23 -0700

    Updated localization strings

--- Pod/Classes/Localization.swift
+++ Pod/Classes/Localization.swift
@@ -10,11 +10,15 @@ import Foundation
 
 public enum Localization: String {
     case StringsFileName = "Localizable"
+    
     case AccessoryButtonAccessibilityLabel = "ACCESSORY_BUTTON_ACCESSIBILITY_LABEL"
     case NumberInputTextFieldAccessibilityLabel = "NUMBER_INPUT_TEXTFIELD_ACCESSIBILITY_LABEL"
     case CVCInputTextFieldAccessibilityLabel = "CVC_TEXTFIELD_ACCESSIBILITY_LABEL"
     case MonthInputTextFieldAccessibilityLabel = "MONTH_INPUT_TEXTFIELD_ACCESSIBILITY_LABEL"
     case YearInputTextFieldAccessibilityLabel = "YEAR_INPUT_TEXTFIELD_ACCESSIBILITY_LABEL"
+    
+    case invalidCardNumber = "INVALID_CARD_NUMBER"
+    case invalidExpirationDate = "INVALID_EXPIRATION_DATE"
     
     static func accessibilityLabelForTextField(textField: UITextField) -> String? {
         switch textField {
